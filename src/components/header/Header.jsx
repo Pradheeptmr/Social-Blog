2a21dd7bc55b7b62788fc1ded9ab34f73b563e01
Pradheeptmr <110891337+Pradheeptmr@users.noreply.github.com>
2023-02-18 09:24:56 +0530

    Add files via upload

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import "./header.css"
+
+export default function Header ()  {
+  return (
+    <div className="header">
+        <div className="headerTitles">
+            <span className="headerTitleSm">Business coperation</span>
+            <span className="headerTitleLg">Blog</span>
+        </div>
+        <img
+        className="headerImg"
+        src="https://media.istockphoto.com/id/1454530397/photo/successful-partnership.jpg?s=612x612&w=0&k=20&c=jhJ1sCzcR-F_A02q4Zy9s57K6MIPqcGgXbV1uNft1Jc="
+        alt=""
+      />
+
+      
+    </div>
+  )
+}
+
---
+++ src/components/post/Post.jsx
@@ -0,0 +1,119 @@
+import "./post.css"
+
+export default function Post  ()  {
+  return (
+    <>
+    <div className="post">
+      <img className="postImg"
+        src="https://images.pexels.com/photos/9301878/pexels-photo-9301878.jpeg?auto=compress&cs=tinysrgb&w=400"
+        alt="" />
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">Music</span>
+                <span className="postCat">Life</span>
+            </div>
+            <span className="postTitle">
+               Lorem ipsum dolor sit amet
+            </span>
+            <hr/>
+            <span className="postDate">1 hour ago</span>
+        </div>
+        <p className="postDesc">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+        atque, exercitationem quibusdam, reiciendis odio laboriosam?
+      </p>
+    </div>
+    <div className="post">
+      <img className="postImg"
+        src="https://images.pexels.com/photos/15388654/pexels-photo-15388654.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+        alt="" />
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">Music</span>
+                <span className="postCat">Life</span>
+            </div>
+            <span className="postTitle">
+               Lorem ipsum dolor sit amet
+            </span>
+            <hr/>
+            <span className="postDate">1 hour ago</span>
+        </div>
+        <p className="postDesc">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+        atque, exercitationem quibusdam, reiciendis odio laboriosam?
+      </p>
+    </div>
+    <div className="post">
+      <img className="postImg"
+        src="https://images.pexels.com/photos/10352348/pexels-photo-10352348.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+        alt="" />
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">Music</span>
+                <span className="postCat">Life</span>
+            </div>
+            <span className="postTitle">
+               Lorem ipsum dolor sit amet
+            </span>
+            <hr/>
+            <span className="postDate">1 hour ago</span>
+        </div>
+        <p className="postDesc">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+        atque, exercitationem quibusdam, reiciendis odio laboriosam?
+      </p>
+    </div>
+    <div className="post">
+      <img className="postImg"
+        src="https://images.pexels.com/photos/11013190/pexels-photo-11013190.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+        alt="" />
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">Music</span>
+                <span className="postCat">Life</span>
+            </div>
+            <span className="postTitle">
+               Lorem ipsum dolor sit amet
+            </span>
+            <hr/>
+            <span className="postDate">1 hour ago</span>
+        </div>
+        <p className="postDesc">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+        atque, exercitationem quibusdam, reiciendis odio laboriosam?
+      </p>
+    </div>
+    <div className="post">
+      <img className="postImg"
+        src="https://images.pexels.com/photos/9825820/pexels-photo-9825820.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+        alt="" />
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">Music</span>
+                <span className="postCat">Life</span>
+            </div>
+            <span className="postTitle">
+               Lorem ipsum dolor sit amet
+            </span>
+            <hr/>
+            <span className="postDate">1 hour ago</span>
+        </div>
+        <p className="postDesc">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+        atque, exercitationem quibusdam, reiciendis odio laboriosam?
+      </p>
+    </div>
+    </>
+    
+  )
+  }
